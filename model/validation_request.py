8c6b69d33f293b1f954e5dddc1b8a80296d9e030
Jordan Idehen <jidehen@Jordans-MacBook-Pro.local>
2025-06-02 16:39:49 -0400

    cleanup

--- model/validation_request.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class ValidationRequest(BaseModel):
-    user_id: str
-    card_id: str
-    amount: float 
--- model/validation_response.py
+++
@@ -1,6 +0,0 @@
-from pydantic import BaseModel
-
-class ValidationResponse(BaseModel):
-    sufficient_credit: bool
-    daily_limit_ok: bool
-    card_active: bool 
